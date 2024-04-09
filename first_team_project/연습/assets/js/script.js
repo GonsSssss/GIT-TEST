15d1744d506ca1337ee566729b485366ea2ea535
GonsSssss <gmlrhs2@gmail.com>
2024-04-09 23:12:12 +0900

    ㅁㄴㅇㄹ

---
+++ first_team_project/연습/assets/js/script.js
@@ -0,0 +1,11 @@
+(function () {
+  let kvSwiper = new Swiper(`.kvsection .swiper`, {
+    loop: true,
+    autoplay: {
+      delay: 400,
+    },
+  });
+  let otSwiper = new Swiper(`.ourteam-imgbox .swiper`, {
+    loop: true,
+  });
+})();
